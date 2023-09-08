b6da809e42641317478b1e2ed14da472a7cff186
alfarchi <8c.alfan@gmail.com>
2023-09-08 22:13:08 +0700

    New content

--- lib/main.dart
+++ lib/main.dart
@@ -10,28 +10,46 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp (
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Trial',
       home: Scaffold(
         appBar: AppBar(
           title: Text('Flutter Trial'),
-          backgroundColor: Colors.deepOrange,
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.network('https://www.petanikode.com/img/flutter/flutter.png'),
-            Text(
-              'Belajar Flutter di Petani Kode',
-              style: TextStyle (fontSize: 24, fontFamily: "Serif", height: 2.0)
-            ),
-            Text('Pembelajaran Flutter untuk Pemula'),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Image.network('https://www.petanikode.com/img/flutter/flutter-sqr.png', width: 175,),
-                Image.network('https://www.petanikode.com/img/flutter/flutter-sqr.png', width: 175,),
-              ],
+          backgroundColor: Colors.blue,
+          actions: [
+            IconButton(
+              icon: Icon(Icons.mail),
+              onPressed: () {},
+              color: Colors.yellowAccent,
             )
-          ]
+          ],
+        ),
+        body: SafeArea(
+          child: Container(
+            color: Colors.red,
+            margin: EdgeInsets.only(top: 10.0, right: 10.0, left: 10.0),
+            padding: EdgeInsets.only(top: 10.0, right: 10.0, left: 10.0),
+            child: Column(
+              children: <Widget>[
+                Image(image: AssetImage('assets/images/mendar-bouchali-HHIxGdj9m-Q-unsplash.jpg',),
+                // width: 200,
+                ),
+                Image.network('https://www.petanikode.com/img/flutter/flutter.png'),
+                Text(
+                  'Belajar Flutter di Petani Kode',
+                  style: TextStyle (fontSize: 24, fontFamily: "Serif", height: 2.0)
+                ),
+                Text('Pembelajaran Flutter untuk Pemula'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Image.network('https://www.petanikode.com/img/flutter/flutter-sqr.png', width: 175,),
+                    Image.network('https://www.petanikode.com/img/flutter/flutter-sqr.png', width: 175,),
+                  ],
+                )
+              ]
+            )
+          )
         )
       )
     );
