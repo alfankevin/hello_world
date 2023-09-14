9651ed79c7fe4b4b99a2f4860784f986d23e0cdc
alfarchi <8c.alfan@gmail.com>
2023-09-14 22:41:35 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,238 +7,63 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp (
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity
+      ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('MyApp'),
-          backgroundColor: Colors.red,
+          title: Text('Konverter Suhu'),
         ),
-        // body: Row(
-        // body: Column(
-        // body: Stack(
-        body: ListView(
-        // body: GridView.count(
-        //   crossAxisCount: 3,
-          children: <Widget>[
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                color: Colors.white,
-                child: Row(
-                  children: [
+        body: Container(
+          margin: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Masukkan suhu dalam Celcius'
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+                ]
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
                   Expanded(
                     flex: 1,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        elevation: MaterialStateProperty.all<double>(0.0),
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                        minimumSize: MaterialStateProperty.all<Size>(
-                          Size(0, 50)
-                        ),
-                      ),
-                      child: Text('Berita Terbaru'.toUpperCase(), style: TextStyle(color: Colors.black,)),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ButtonStyle(
-                        elevation: MaterialStateProperty.all<double>(0.0),
-                        backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
-                        minimumSize: MaterialStateProperty.all<Size>(
-                          Size(0, 50)
-                        ),
-                      ),
-                      child: Text('Pertandingan Hari Ini'.toUpperCase(), style: TextStyle(color: Colors.black,)),
-                    ),
-                  ),
-                ],
-                ),
-              ),
-            ),
-            Container(
-              color: Colors.purple,
-              margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-              padding: EdgeInsets.all(1.0),
-              child: Column(
-                children: [
-                  Container(
-                    child: Image(
-                      image: AssetImage('assets/images/mendar-bouchali-HHIxGdj9m-Q-unsplash.jpg',),
-                    )
-                  ),
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(10),
-                    color: Colors.white,
-                    child: Text('Costa Mendekat Ke Palmeiras', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold,),),
-                  ),
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.only(left: 10.0, top: 15.0, bottom: 15.0),
-                    color: Colors.purpleAccent,
-                    child: Text('Transfer', style: TextStyle(color: Colors.black, fontSize: 14.0),),
-                  ),
-                ],
-              )
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1,
-                ),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.black,
-                          width: 1,
-                        ),
-                      ),
-                    ),
-                    child: Row(
+                    child: Column(
                       children: [
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            child: Image(
-                              image: AssetImage('assets/images/mendar-bouchali-HHIxGdj9m-Q-unsplash.jpg',),
-                            )
-                          )
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            color: Colors.white,
-                            child: Text('Pique Bilang Wasit Untung-kan Madrid, Koeman Tepok', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 14.0),),
-                          )
-                        ),
+                        Text('Reamur'),
+                        Text('0'),
                       ],
                     )
                   ),
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(10.0),
-                    color: Colors.white,
-                    child: Text('Barcelona Feb 13, 2021', style: TextStyle(color: Colors.black, fontSize: 14.0),),
-                  ),
-                ],
-              )
-            ),
-            Container(
-              margin: EdgeInsets.only(left: 10.0, right: 10.0, bottom: 10.0),
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1,
-                ),
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border(
-                        bottom: BorderSide(
-                          color: Colors.black,
-                          width: 1,
-                        ),
-                      ),
-                    ),
-                    child: Row(
+                  Expanded(
+                    flex: 1,
+                    child: Column(
                       children: [
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            child: Image(
-                              image: AssetImage('assets/images/mendar-bouchali-HHIxGdj9m-Q-unsplash.jpg',),
-                            )
-                          )
-                        ),
-                        Expanded(
-                          flex: 1,
-                          child: Container(
-                            color: Colors.white,
-                            child: Text('Pique Bilang Wasit Untung-kan Madrid, Koeman Tepok', textAlign: TextAlign.center, style: TextStyle(color: Colors.black, fontSize: 14.0),),
-                          )
-                        ),
+                        Text('Reamur'),
+                        Text('0'),
                       ],
-                    )
-                  ),
-                  Container(
-                    width: double.infinity,
-                    padding: EdgeInsets.all(10.0),
-                    color: Colors.white,
-                    child: Text('Barcelona Feb 13, 2021', style: TextStyle(color: Colors.black, fontSize: 14.0),),
+                    ),
                   ),
                 ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('asd')
+                )
               )
-            ),
-            Container(
-              color: Colors.black,
-              alignment: Alignment.bottomCenter,
-              child: FlutterLogo(size: 50.0),
-            ),
-            // Container(
-            //   color: Colors.red,
-            //   alignment: Alignment.bottomCenter,
-            //   child: Text("Dua", style: TextStyle(fontSize: 30.0, color: Colors.white)),
-            //   height: 400,
-            //   width: 300,
-            // ),
-            // Container(
-            //   color: Colors.deepPurple,
-            //   alignment: Alignment.bottomCenter,
-            //   child: Text("Tiga", style: TextStyle(fontSize: 30.0, color: Colors.white)),
-            //   height: 200,
-            //   width: 200,
-            // ),
-            // Container(
-            //   color: Colors.blue,
-            //   alignment: Alignment.bottomCenter,
-            //   child: Text("Tiga", style: TextStyle(fontSize: 30.0, color: Colors.white)),
-            //   height: 200,
-            //   width: 200,
-            // ),
-            // Container(
-            //   color: Colors.green,
-            //   alignment: Alignment.bottomCenter,
-            //   child: FlutterLogo(size: 50.0)
-            // ),
-            // Container(
-            //   color: Colors.red,
-            //   alignment: Alignment.bottomCenter,
-            //   child: Text("Dua", style: TextStyle(fontSize: 30.0, color: Colors.white)),
-            //   height: 400,
-            //   width: 300,
-            // ),
-            // Container(
-            //   color: Colors.deepPurple,
-            //   alignment: Alignment.bottomCenter,
-            //   child: Text("Tiga", style: TextStyle(fontSize: 30.0, color: Colors.white)),
-            //   height: 200,
-            //   width: 200,
-            // ),
-            // Container(
-            //   color: Colors.blue,
-            //   alignment: Alignment.bottomCenter,
-            //   child: Text("Tiga", style: TextStyle(fontSize: 30.0, color: Colors.white)),
-            //   height: 200,
-            //   width: 200,
-            // ),
-          ]
+            ],
+          ),
         )
       )
     );
