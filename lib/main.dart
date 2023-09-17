7b5428f8542ed38e9309a6507b7696bbcf4a16fc
alfarchi <8c.alfan@gmail.com>
2023-09-17 17:54:30 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -15,74 +15,106 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Konverter Suhu'),
-        ),
-        body: Container(
-          margin: EdgeInsets.all(8),
-          child: Column(
-            children: [
-              Expanded(
-                flex: 1,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Masukkan suhu Dalam Celcius'
+      home: MyCustomApp(),
+    );
+  }
+}
+
+class MyCustomApp extends StatefulWidget {
+  const MyCustomApp({super.key});
+
+  @override
+  State<MyCustomApp> createState() => _MyCustomAppState();
+}
+
+class _MyCustomAppState extends State<MyCustomApp> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Konverter Suhu'),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Masukkan suhu Dalam Celcius'
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
+                ],
+                controller: myController,
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Suhu dalam Kelvin', style: TextStyle(fontSize: 16),),
+                        SizedBox(height: 20),
+                        Text('150', style: TextStyle(fontSize: 40),),
+                      ],
+                    )
                   ),
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    FilteringTextInputFormatter.allow(RegExp(r'[0-9]'))
-                  ]
-                ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text('Suhu dalam Reamur', style: TextStyle(fontSize: 16),),
+                        SizedBox(height: 20),
+                        Text('200', style: TextStyle(fontSize: 40),),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              Expanded(
-                flex: 1,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Suhu dalam Kelvin', style: TextStyle(fontSize: 16),),
-                          SizedBox(height: 20),
-                          Text('150', style: TextStyle(fontSize: 40),),
-                        ],
-                      )
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text('Suhu dalam Reamur', style: TextStyle(fontSize: 16),),
-                          SizedBox(height: 20),
-                          Text('200', style: TextStyle(fontSize: 40),),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Expanded(
-                flex: 1,
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ButtonStyle(
-                      minimumSize: MaterialStateProperty.all(
-                        Size(double.infinity, 50)
-                      )
-                    ),
-                    child: Text('Konversi Suhu')
-                  )
+            ),
+            Expanded(
+              flex: 1,
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: ElevatedButton(
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          content: Text(myController.text),
+                        );
+                      },
+                    );
+                  },
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all(
+                      Size(double.infinity, 50)
+                    )
+                  ),
+                  child: Text('Konversi Suhu')
                 )
               )
-            ],
-          ),
-        )
+            )
+          ],
+        ),
       )
     );
   }
